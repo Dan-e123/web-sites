f2c0b1790e88b372c18552e74fbd6326ad7e05e8
itaiagiv <itaiagiv@gmail.com>
2025-12-02 16:50:14 +0200

    לוח הכפל

---
+++ DanElalWebsite/AspDotNet.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AspDotNet : System.Web.UI.Page
+{
+    public string strkefel;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        strkefel = "<table border= '1' width= 100%  >";
+        for (int i = 1; i <= 10; i++)
+        {
+            if(i%2==0)
+            strkefel += "<tr style= 'background-Color:blue'>";
+            else
+                strkefel += "<tr style= 'background-Color:pink'>";
+            for (int j = 1; j <= 10; j++)
+            {
+                strkefel += "<td>";
+                strkefel += i * j;
+                strkefel += "</td>";
+            }
+            strkefel += "</tr>";
+        }
+        strkefel += "</table>";
+
+    }
+}
